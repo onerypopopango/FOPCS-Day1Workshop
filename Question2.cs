b9dcd547ce407483eb97442f8dd4c43e697aefcb
Onery <onery.popopango@gmail.com>
2018-02-10 17:54:40 +0800

    Init Upload
    Initial uploading

---
+++ Question2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace Day1Workshop
+{
+    class Question2
+    {
+        static void Main()
+        {
+            string name;
+
+            Console.Write("Hello earthling, what might your name be: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Well, good morning " + name + "!");
+        }
+    }
+}
---
+++ Question3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Day1Workshop
+{
+    class Question3
+    {
+        static void Main()
+        {
+            string num;
+
+            Console.Write("Hello earthling, pass me a number. Any number: ");
+            num = Console.ReadLine();
+
+            int numb = Int32.Parse(num);
+            int square = numb * numb;
+
+            Console.WriteLine("The square of {0} is {1}", numb, square);
+        }
+    }
+}
